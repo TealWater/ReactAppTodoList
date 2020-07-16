9a7d0f557a6afb3e8c063f4585225785bae1464f
Matthew Straughn <matthewe99@hotmail.com>
2020-07-16 18:58:50 -0400

    Updates

---
+++ precache-manifest.586b9521e904c7a20ba8d198594bdf06.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6f7853eea0fff3ac68a0c21c731e3e2",
+    "url": "/ReactAppTodoList/index.html"
+  },
+  {
+    "revision": "6d9fd93d9bdaeaf46db9",
+    "url": "/ReactAppTodoList/static/css/main.bdacf6c2.chunk.css"
+  },
+  {
+    "revision": "bf4bbb13826b034f6d14",
+    "url": "/ReactAppTodoList/static/js/2.3da1bb44.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ReactAppTodoList/static/js/2.3da1bb44.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d9fd93d9bdaeaf46db9",
+    "url": "/ReactAppTodoList/static/js/main.2c3356cd.chunk.js"
+  },
+  {
+    "revision": "4c133215e484a98b166b",
+    "url": "/ReactAppTodoList/static/js/runtime-main.44b6b118.js"
+  }
+]);
